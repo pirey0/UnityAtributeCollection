c907ef8de765cb42996473228307d40e06b64639
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-10-03 23:10:26 +0200

    added [Button] to make clickable buttons in Inspector, added [Label] to make atributes read only in Inspector

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [Label] [SerializeField] private float exampleLabel = 3.14159f;
+    
+    [Button]
+    public void TestMethod ()
+    {
+        Debug.Log("TEST");
+    }
+
+    [Button]
+    public bool TestParameter( bool flag, string str)
+    {
+        Debug.Log(str + ": " + flag);
+        return flag;
+    }
+    
+}
